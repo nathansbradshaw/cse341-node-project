6c37adec169ed5cf8cb080fa0dd9aedf12b646e3
Nathan Bradshaw <nathansbradshaw@gmail.com>
2021-01-30 17:11:09 -0700

    prove
    lots of changes

--- routes/shop.js
+++ routes/shop.js
@@ -17,10 +17,9 @@ router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-router.post('/cart-add-item', shopController.postCart);
+
+router.post('/create-order', shopController.postOrder);
 
 router.get('/orders', shopController.getOrders);
-
-router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
